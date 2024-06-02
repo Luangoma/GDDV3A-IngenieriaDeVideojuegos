a27acede7f67d50e63f0e7450603fab6b19ee1c4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-06-02 21:39:14 +0200

    Created a camera target component
    This camera target component will be used by the camera manager to determine what targets the camera will follow during runtime.

---
+++ IngenieriaDeVideojuegos/Assets/Scripts/Utility/CameraTarget.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] public Transform cameraSocket;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
