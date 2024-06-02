0d41d4cc8c949fc76cf06a2df2c6ae124ae56335
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-06-03 00:41:21 +0200

    Added basic player controller

---
+++ IngenieriaDeVideojuegos/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private Rigidbody rigidBody;
+
+    [SerializeField] private float linearAcceleration;
+    [SerializeField] private float angularAcceleration;
+    
+    [SerializeField] private float maxLinearVelocity;
+    [SerializeField] private float maxAngularVelocity;
+
+    private Vector2 movementVector;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateMovement(Time.fixedDeltaTime);
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+
+    #endregion
+
+    #region PublicMethods
+    #endregion
+
+    #region PrivateMethods
+
+    private void GetInput()
+    {
+        movementVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)) movementVector.x += 1;
+        if (Input.GetKey(KeyCode.S)) movementVector.x -= 1;
+
+        if (Input.GetKey(KeyCode.A)) movementVector.y += 1;
+        if (Input.GetKey(KeyCode.D)) movementVector.y -= 1;
+    }
+
+    private void UpdateMovement(float delta)
+    {
+        if (this.rigidBody.velocity.magnitude < this.maxLinearVelocity)
+            this.rigidBody.AddForce(delta * this.movementVector.x * this.transform.up * this.linearAcceleration, ForceMode.VelocityChange);
+
+        if (this.rigidBody.angularVelocity.magnitude < this.maxAngularVelocity)
+            this.rigidBody.AddTorque(delta * this.movementVector.y * this.transform.forward * this.angularAcceleration, ForceMode.VelocityChange);
+    }
+
+    #endregion
+
+}
