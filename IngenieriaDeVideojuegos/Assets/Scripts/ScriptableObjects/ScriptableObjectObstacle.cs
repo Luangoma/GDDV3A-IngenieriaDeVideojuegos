d659320eb0c83a18c75ba5144adc028c2d0f8a31
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-06-15 08:52:42 +0200

    Modified obstacle scriptable object
    The scriptable object for asteroid now makes use of the asteroid type enum to return data depending on what type the asteroid is.
    
    Each asteroid / obstacle of each type has its own speed and sprites, as well as their own behaviour (asteroid type specific behaviour is to be implemented within the asteroid class itself).
    
    The target variable has now been made private to prevent it from appearing on the editor's scriptable object menu. The target is now accessible through a getter and setter.

--- IngenieriaDeVideojuegos/Assets/Scripts/ScriptableObjects/ScriptableObjectObstacle.cs
+++ IngenieriaDeVideojuegos/Assets/Scripts/ScriptableObjects/ScriptableObjectObstacle.cs
@@ -5,8 +5,88 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Obstacle", menuName = "Obstacle")]
 public class ScriptableObjectObstacle : ScriptableObject
 {
-    // Elementos comunes a todos los obstaculos de cierto tipo
-    // public string objectName;
-    public GameObject target;
-    public float speed = 20;
+    #region Variables
+
+    // Elementos comunes a todos los obstaculos de cierto tipo.
+
+    private GameObject target;
+
+    // Speeds:
+    [Header("Speed Data")]
+    [SerializeField] private float defaultSpeed = 20;
+    [SerializeField] private float projectileSpeed = 20;
+    [SerializeField] private float explosiveSpeed = 20;
+    [SerializeField] private float shooterSpeed = 20;
+
+    // Sprites (ON):
+    [Header("Sprites (ON)")]
+    [SerializeField] private Sprite defaultSpriteOn;
+    [SerializeField] private Sprite projectileSpriteOn;
+    [SerializeField] private Sprite explosiveSpriteOn;
+    [SerializeField] private Sprite shooterSpriteOn;
+
+    // Sprites (OFF):
+    [Header("Sprites (OFF)")]
+    [SerializeField] private Sprite defaultSpriteOff;
+    [SerializeField] private Sprite projectileSpriteOff;
+    [SerializeField] private Sprite explosiveSpriteOff;
+    [SerializeField] private Sprite shooterSpriteOff;
+
+
+    #endregion
+
+    #region Getters
+
+    public GameObject Target { get { return target; } set { target = value; } }
+
+    #endregion
+
+    #region PublicMethods
+
+    public float GetSpeed(AsteroidType type)
+    {
+        switch (type)
+        {
+            default:
+                return defaultSpeed;
+            case AsteroidType.Projectile:
+                return projectileSpeed;
+            case AsteroidType.Explosive:
+                return explosiveSpeed;
+            case AsteroidType.Shooter:
+                return shooterSpeed;
+        }
+    }
+
+    public Sprite GetSpriteOn(AsteroidType type)
+    {
+        switch (type)
+        {
+            default:
+                return defaultSpriteOn;
+            case AsteroidType.Projectile:
+                return projectileSpriteOn;
+            case AsteroidType.Explosive:
+                return explosiveSpriteOn;
+            case AsteroidType.Shooter:
+                return shooterSpriteOn;
+        }
+    }
+
+    public Sprite GetSpriteOff(AsteroidType type)
+    {
+        switch (type)
+        {
+            default:
+                return defaultSpriteOff;
+            case AsteroidType.Projectile:
+                return projectileSpriteOff;
+            case AsteroidType.Explosive:
+                return explosiveSpriteOff;
+            case AsteroidType.Shooter:
+                return shooterSpriteOff;
+        }
+    }
+
+    #endregion
 }
